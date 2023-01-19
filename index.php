a7e4056965030e44f5e6048b7a0d33a221bb8eb8
Himu369 <98264360+Himu369@users.noreply.github.com>
2023-01-19 14:26:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery-3.6.0.min.js" type="text/javascript"></script>
+		<link rel="stylesheet"type="text/css" href="style.css">
+ <title>Bharat Coking Coal Limited</title> 
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+		<nav>
+		<img src="bccllogo1-1.png"/>
+		<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="login.php">Login</a></li>
+	    <li><a href="#">About</a></li>
+		<li><a href="#">Contact</a></li>
+		<li><a href="#">Exit</a></li>
+	    </ul>
+		</nav>
+		 <br><br><br>
+		 <div><h1 style="text-align:center"><p style="color:red">LOST AND FOUND </p> </h1>
+				<b> <p style="text-align:center; color:green">This portal is for the general management of lost&found items in the BCCL Koylabhawan premises. </p></b>
+				</div>
+        <div class="container">
+              <div class="col-lg-9 column_style">
+                <br><h3><a href="">BCCL: Bharat Coking Coal Limited</a></h3>
+                <b>BCCL was incorporated in January, 1972 to operate coking coal mines operating in the Jharia & Raniganj Coalfields,
+				taken over by the Govt. of India on 16th Oct,1971 to ensure planned development of the scarce coking coal resources in the country.
+				It is a Public Sector Undertaking engaged in mining of coal and allied activities. It occupies an important place in as much as it 
+				produces bulk of the coking coal mined in the country.
+				</b>   
+				 </div>
+ 
+						 <h3><a href= "https://csrbox.org/India_Company_Bharat-Coking-Coal-Limited-Jharkhand_6032">Bharat Coking Coal Limited : Mining Mineral products
+		</a><b></h3>
+				 Bharat Coking Coal Limited (BCCL) is a Public Sector Undertaking engaged in mining of coal and allied activities. It occupies an important place in as
+				 much as it produces bulk of the coking coal mined in the country. BCCL meets almost 50% of the total prime coking coal requirement of the integrated steel 
+				 sector.  </b>
+             </div>		
+               	 
+			 </div>
+			
+</body>
+</html>
